1c637545c35d32bd48d94e865236b1bd00df082e
Patrick LEFEVRE <patrick.lefevre@gmail.com>
2017-07-16 10:10:33 +0200

    Update pat_eu_cookies_law.php
    Latest code version.

--- pat_eu_cookies_law.php
+++ pat_eu_cookies_law.php
@@ -43,8 +43,12 @@ function pat_eu_cookies_law($atts, $thing = null) {
 
 		'lang'       => $prefs['language'],
 		'duration'   => '1 Month',
+		'force_reload' => false
 
 	), $atts));
+
+	// js expression for force reload on demand
+	$force = ($force_reload ? "window.location='';" : "");
 
 	// The default string entries for international translation
 	$default = array(
@@ -70,7 +74,7 @@ function pat_eu_cookies_law($atts, $thing = null) {
 	}
 
 
-	return '<div class="pat_eu_cookies_law"><div id="msg-cookies"></div> <p id="cookie-choices">'.$default['msg'].'<br /><span id="cookies-delay">'.$default['remind'].' <strong id="counter">1:00</strong></span></p></div>'.n._pat_eu_cookies_law_inject( $default['refuse'], $default['no_allowed'], $duration );
+	return '<div class="pat_eu_cookies_law"><div id="msg-cookies"></div> <p id="cookie-choices">'.$default['msg'].'<br /><span id="cookies-delay">'.$default['remind'].' <strong id="counter">1:00</strong></span></p></div>'.n._pat_eu_cookies_law_inject( $default['refuse'], $default['no_allowed'], $duration, $force );
 
 }
 
@@ -87,14 +91,21 @@ function _pat_eu_cookies_law_inject($refuse, $no_allowed, $future) {
 
 	global $prefs;
 
-	// Two variables for convenient insertion into js
-	$countries = $prefs['pat_eu_cookies_law_countries'];
-	$files = $prefs['pat_eu_cookies_law_js'];
+	// Variable convertion for convenient insertion into js. Removes all spaces
+	$list = preg_replace('/\s+/', '', $prefs['pat_eu_cookies_law_js']);
+	// Converts to an array
+	$list = explode(',', $list);
+	// Affects each quoted values
+	foreach($list as $key => $value) {
+		$list[$key] = '"'.$value.'"';
+	}
+	// $files is a list of quoted strings
+	$files = implode($list, ',');
 
 	$out = <<<EOJ
 <script>
 /*! Simple EU Cookies Law Compliance without dependencies by cara-tm.com, 2017. MIT license - https://github.com/cara-tm/EU-Cookies-Law-Compliance/ */
-function EU_cookies_law(r){'use strict';var msg='$refuse',future='$future',minutes=1,no_alowed_cookies='$no_allowed',domain=window.location.hostname,lang=(navigator.language||navigator.browserLanguage),countries=[$countries],affected=0,seconds=60,mins=minutes,accept_cookies=document.getElementById('ok-cookies'),refuse_cookies=document.getElementById('no-cookies');if(false!==navigator.cookieEnabled){for(var i=0;i<countries.length;i++){if(countries[i]===lang.substring(0,2).toUpperCase()){affected=1;break;}}if(affected==0){sanitize_msg('');jsloader(r);}else check_cookies();accept_cookies.onclick=function(evt){evt.preventDefault();launch(evt);};function launch(){future=parseInt(future.substring(0,1));var expires=new Date(new Date().setMonth(new Date().getMonth()+future));cookie_creation('Ok',expires);jsloader(r);sanitize_msg('');}refuse_cookies.onclick=function(evt){var tomorrow=new Date(new Date().setDate(new Date().getDate()+1));cookie_creation('No',tomorrow);sanitize_msg(msg);evt.preventDefault();window.location='';};function getCookie(sName){var oRegex=new RegExp('(?:; )?'+sName+'=([^;]*);?');if(oRegex.test(document.cookie)) return decodeURIComponent(RegExp.$1);else return null;}function check_cookies(){tick();if(getCookie(domain)==='Ok'+domain){sanitize_msg('');jsloader(r);}else if(getCookie(domain)==='No'+domain){sanitize_msg(msg);}}function cookie_creation(c,e){return document.cookie=domain+'='+encodeURIComponent(c+domain)+';expires='+e.toGMTString();}function jsloader(r){var s=[];var a=document.getElementsByTagName("script")[0];if(!window.scriptHasRun){window.scriptHasRun=true;for(var i=0;i<r.length;i++){if(r[i]!==0||!window.scriptHasRun){window.scriptHasRun=true;s[i]=document.createElement('script');s[i].src=r[i];document.getElementsByTagName('head')[0].appendChild(s[i])||a.parentNode.insertBefore(s[i],a);}}}}function tick(){if(minutes!=0&&null!==document.getElementById('counter')){var counter=document.getElementById('counter'),current_minutes=mins-1;seconds--;if(typeof counter.innerHTML!==null)counter.innerHTML=current_minutes.toString()+':'+(seconds<10?'0':'')+String(seconds);if(seconds>0){setTimeout(tick,1000);}else{if(mins>1){countdown(mins-1);}}if(seconds==0){launch();sanitize_msg('');}}else{null!==document.getElementById('cookies-delay')?document.getElementById('cookies-delay').innerHTML='':'';}}}else{sanitize_msg(no_alowed_cookies);}function sanitize_msg(m){document.getElementById("cookies-delay").innerHTML='';return document.getElementById('cookie-choices').innerHTML=m;}};
+function EU_cookies_law(r){'use strict';var msg='$refuse',future='$future',minutes=1,no_alowed_cookies='$no_allowed',domain=window.location.hostname,lang=(navigator.language||navigator.browserLanguage),countries=[{$prefs['pat_eu_cookies_law_countries']}],affected=0,seconds=60,mins=minutes,accept_cookies=document.getElementById('ok-cookies'),refuse_cookies=document.getElementById('no-cookies');if(false!==navigator.cookieEnabled){for(var i=0;i<countries.length;i++){if(countries[i]===lang.substring(0,2).toUpperCase()){affected=1;break;}}if(affected==0){sanitize_msg('');jsloader(r);}else check_cookies();accept_cookies.onclick=function(evt){evt.preventDefault();launch(evt);$force};function launch(){future=parseInt(future.substring(0,1));var expires=new Date(new Date().setMonth(new Date().getMonth()+future));cookie_creation('Ok',expires);jsloader(r);sanitize_msg('');}refuse_cookies.onclick=function(evt){evt.preventDefault();var tomorrow=new Date(new Date().setDate(new Date().getDate()+1));cookie_creation('No',tomorrow);sanitize_msg(msg);window.location='';};function getCookie(sName){var oRegex=new RegExp('(?:; )?'+sName+'=([^;]*);?');if(oRegex.test(document.cookie)) return decodeURIComponent(RegExp.$1);else return null;}function check_cookies(){tick();if(getCookie(domain)==='Ok'+domain){sanitize_msg('');jsloader(r);}else if(getCookie(domain)==='No'+domain){sanitize_msg(msg);}}function cookie_creation(c,e){return document.cookie=domain+'='+encodeURIComponent(c+domain)+';expires='+e.toGMTString();}function jsloader(r){var s=[];var a=document.getElementsByTagName("script")[0];if(!window.scriptHasRun){window.scriptHasRun=true;for(var i=0;i<r.length;i++){if(r[i]!==0||!window.scriptHasRun){window.scriptHasRun=true;s[i]=document.createElement('script');s[i].src=r[i];document.getElementsByTagName('head')[0].appendChild(s[i])||a.parentNode.insertBefore(s[i],a);}}}}function tick(){if(minutes!=0&&null!==document.getElementById('counter')){var counter=document.getElementById('counter'),current_minutes=mins-1;seconds--;if(typeof counter.innerHTML!==null)counter.innerHTML=current_minutes.toString()+':'+(seconds<10?'0':'')+String(seconds);if(seconds>0){setTimeout(tick,1000);}else{if(mins>1){countdown(mins-1);}}if(seconds==0){launch();sanitize_msg('');}}else{null!==document.getElementById('cookies-delay')?document.getElementById('cookies-delay').innerHTML='':'';}}}else{sanitize_msg(no_alowed_cookies);}function sanitize_msg(m){document.getElementById("cookies-delay").innerHTML='';return document.getElementById('cookie-choices').innerHTML=m;}};
 /*! Array of third part JS files to load */ 
 EU_cookies_law([$files]);
 </script>
@@ -115,14 +126,14 @@ function pat_eu_cookies_law_prefs() {
 
 	global $textarray;
 
-	$textarray['pat_eu_cookies_law_countries'] = 'List of EU country members code';
+	$textarray['pat_eu_cookies_law_countries'] = 'List of EU country members codes';
 	$textarray['pat_eu_cookies_law_js'] = 'List of javascript files to load';
 
 	if ( !safe_field('name', 'txp_prefs', "name='pat_eu_cookies_law_countries'") ) {
 		safe_insert('txp_prefs', "name='pat_eu_cookies_law_countries', val='\'AT\',\'BE\',\'BG\',\'HR\',\'CZ\',\'CY\',\'DK\',\'EE\',\'FI\',\'FR\',\'DE\',\'EL\',\'HU\',\'IE\',\'IT\',\'LV\',\'LT\',\'LU\',\'MT\',\'NL\',\'PL\',\'PT\',\'SK\',\'SI\',\'ES\',\'SE\',\'GB\',\'UK\'', type=1, event='admin', html='text_input', position=21");
 	}
 	if ( !safe_field('name', 'txp_prefs', "name='pat_eu_cookies_law_js'") ) {
-		safe_insert('txp_prefs', "name='pat_eu_cookies_law_js', val='js/file.js', type=1, event='admin', html='text_input', position=22");
+		safe_insert('txp_prefs', "name='pat_eu_cookies_law_js', val=".hu."'js/file.js', type=1, event='admin', html='text_input', position=22");
 	}
 
 }
